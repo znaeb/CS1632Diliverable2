6d4281cae8e522488a54bff98d8a8a7013d240a7
znaeb <urnotbeanz@gmail.com>
2016-06-06 19:18:59 -0400

    started framework?

---
+++ src/Location.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Location {
+    private String name;
+    private int[]destinations;
+    //destinations[0] where fourth Ave. leads to from current.
+    //destinations[1] is for fifth Ave.  destinations[2] is for Bill St. 
+    // destinations[3] is for Phil St.
+    //returns -1 for no path
+}
